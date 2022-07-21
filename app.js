c8aa4b68fbfdb00b24a7b007dccab47cce7a5e3e
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-07-21 14:30:58 +0300

    Add a for..loop and a function to click to change the active button.

--- app.js
+++ app.js
@@ -5,5 +5,11 @@ const allSections = document.querySelector('.main-content');
 
 
 function pageTransition() {
-    
+    for(let i = 0; i < sectBtn.length; i++) {
+        sectBtn[i].addEventListener('click', function() {
+            let curentBtn = document.querySelectorAll('.active-btn');
+            curentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn'
+        })
+    }
 }
