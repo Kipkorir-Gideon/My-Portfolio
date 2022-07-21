836cf45d19154a707bfc5d6856503335cf1d5c16
Kipkorir-Gideon <gidaliosventures@gmail.com>
2022-07-21 13:42:45 +0300

    Create a function for active button click.

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
+
+
+function pageTransition() {
+    
+}
